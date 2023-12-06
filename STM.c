b7a6f2523fbfaff21895df32981d24f787a230ab
Andre Rauber Du Bois <rauberdubois@gmail.com>
2023-12-05 23:33:02 -0300

    prints

--- STM.c
+++ STM.c
@@ -1,6 +1,9 @@
 #include "STM.h"
+#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+int tr_id_gen=0;
 
 STMData* STM_start(int numObjects, int numTransactions, int numLocators)
 {
@@ -18,13 +21,14 @@ STMData* STM_start(int numObjects, int numTransactions, int numLocators)
 }
 
 TX_Data* TX_Init(STMData* stm_data){
-    unsigned int tx_id = 0;
-
+    unsigned int tx_id = tr_id_gen;
+    tr_id_gen++;
     TX_Data *d = &stm_data -> tx_data[tx_id];
     d-> tr_id = tx_id;
     d-> next_locator = 0;
     d -> read_set.size =0;
     d -> write_set.size = 0;
+    stm_data -> tr_state[d->tr_id] = ACTIVE;
     return d;
 }
 
@@ -33,6 +37,8 @@ Locator* TX_new_locator(STMData* stm_data, TX_Data* tx_data)
   Locator* locator = stm_data -> locators;
   locator += (tx_data->tr_id * stm_data -> num_locators) + tx_data-> next_locator;
   tx_data -> next_locator++;
+  if(tx_data -> next_locator == MAX_LOCATORS)
+     printf("Max locators reached");
   return locator;
 }
 
@@ -42,6 +48,7 @@ int TX_validate_readset(STMData* stm_data, TX_Data* tx_data)
   int size = tx_data-> read_set.size;
   //int* t = read_set -> locator[size]->new_version;
   //int* t = read_set -> value[size];
+  
   for (int i=0;i<size;i++)
   {
        if(stm_data -> vboxes[read_set->object[size]] == read_set -> locator[size])
@@ -134,7 +141,7 @@ int TX_Open_Read(STMData* stm_data, TX_Data* tx_data, uint object)
                 printf("TX_Read: invalid tr state!\n");
                 exit(0);
           }
-    
+
     ReadSet* read_set = &tx_data-> read_set;
     int size = tx_data-> read_set.size;
     read_set -> locator[size] = locator;
@@ -143,7 +150,7 @@ int TX_Open_Read(STMData* stm_data, TX_Data* tx_data, uint object)
     read_set -> size ++;
     if(TX_validate_readset(stm_data,tx_data))
               {
-               return *version;
+                   return *version;
               }
     return 0; 
 }
@@ -176,24 +183,87 @@ for(int i=0;i<total_locators;i++)
   }
 }
 
+void print_vboxes(STMData* stm_data, Locator **vboxes)
+{
+   printf("d");
+}
+
+
+void print_tr_state(int tr_state)
+{
+    switch ( tr_state )
+  {
+    case 1 :
+    printf ("ACTIVE");
+    break;
+    
+    case 2 :
+    printf ("COMMITTED");
+    break;
+    
+    case 3 :
+    printf ("ABORTED");
+    break;
+    
+    default :
+    printf ("Unknown thread state!");
+  }
+}
+void print_locator(STMData* stm_data,Locator *locator)
+{
+  printf("Locator %p:\n", locator);
+  printf("- owner: %d\n",locator->owner);
+  printf("- state: %d, ", stm_data-> tr_state[locator->owner]);
+  print_tr_state(stm_data-> tr_state[locator->owner]);
+  printf("\n");
+  printf("- new_version %d\n", *locator->new_version);
+  printf("- old_version %d\n", *locator->old_version);
+}
+
+
+void* foo(void* p){
+    STMData* stm_data = (STMData*) p;
+    TX_Data* tx_data = TX_Init(stm_data);
+    
+    int value=TX_Open_Read(stm_data,tx_data,0);
+        
+
+    int* ptr_value=TX_Open_Write(stm_data,tx_data,1);
+    *ptr_value = 666;
+    TX_commit(stm_data,tx_data);
+    return NULL;
+    
+}
+
 int main()
 {
   int num_objects = 2;
-  int num_locators = 10;
-  int num_tx = 1;
+  int num_locators = MAX_LOCATORS;
+  int num_tx = 4;
+ 
   STMData* stm_data = STM_start(num_objects, num_tx, num_locators); 
   init_objects(stm_data,num_objects);
-  Locator* loc = stm_data->vboxes[0];
-  
   init_locators(stm_data,num_tx,num_locators);
 
-  TX_Data* tx_data = TX_Init(stm_data);
-  int value=TX_Open_Read(stm_data,tx_data,0);
-  printf("value %d\n",value);
-  int* ptr_value=TX_Open_Write(stm_data,tx_data,1);
-  printf("value %d\n",*ptr_value);
-  TX_commit(stm_data,tx_data);
-  loc = stm_data->vboxes[1];
-  printf("Locator new_value: %d\n",*loc->new_version);
+  pthread_t tid1; 
+  pthread_t tid2; 
+  pthread_t tid3; 
+  pthread_t tid4; 
+  
+  pthread_create(&tid1, NULL, foo, stm_data); 
+  pthread_create(&tid2, NULL, foo, stm_data);
+  pthread_create(&tid3, NULL, foo, stm_data); 
+  pthread_create(&tid4, NULL, foo, stm_data); 
+  
+  pthread_join(tid1, NULL); 
+  pthread_join(tid2, NULL);
+  pthread_join(tid3, NULL); 
+  pthread_join(tid4, NULL); 
+  printf("After Thread\n"); 
+
+  
+  print_locator(stm_data,stm_data->vboxes[0]);
+  print_locator(stm_data,stm_data->vboxes[1]);
+  
   
 }
