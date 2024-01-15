580f50d5c50d1d2ee1ffe77fe1105284ec4772db
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-01-15 09:38:44 -0300

    commit for debuging bank

--- of/STM.cu
+++ of/STM.cu
@@ -326,7 +326,7 @@ __device__ int TX_commit(STMData* stm_data, TX_Data* tx_data)
       //TX_free_writeset(stm_data,tx_data, COMMITTED);
       for(int i=0;i<tx_data->write_set.size;i++)
       {
-      printf("\nLocator: %d, owner: %d, me: %d\n\n",tx_data->write_set.locators[i],stm_data->locators[tx_data->write_set.locators[i]].owner,tx_data->tr_id);
+      //printf("\nLocator: %d, owner: %d, me: %d\n\n",tx_data->write_set.locators[i],stm_data->locators[tx_data->write_set.locators[i]].owner,tx_data->tr_id);
         assert(atomicCAS(&stm_data-> locators[tx_data->write_set.locators[i]].owner,tx_data->tr_id,stm_data-> num_tr)==tx_data->tr_id);
       }
       tx_data -> n_committed ++;
@@ -357,7 +357,7 @@ __device__  int* TX_Open_Write(STMData* stm_data, TX_Data* tx_data, uint object)
       Locator *new_locator = &stm_data -> locators[addr_new_locator];
       new_locator -> owner = tx_data->tr_id;
       new_locator -> object = object;
-      printf("OW: Object %d, Transaction: %d Locator: %d (Owner %d), new Locator %d, next locator %d\n",object,tx_data->tr_id, addr_locator,locator -> owner,addr_new_locator, next_locator);
+    //  printf("OW: Object %d, Transaction: %d Locator: %d (Owner %d), new Locator %d, next locator %d\n",object,tx_data->tr_id, addr_locator,locator -> owner,addr_new_locator, next_locator);
       assert(locator -> owner != new_locator -> owner);
       
       switch (stm_data->tr_state[locator -> owner]) {
