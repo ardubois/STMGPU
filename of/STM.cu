a5c8c1bb36cbc65b19ff971cdac9502f544b76cc
Andre Rauber Du Bois <rauberdubois@gmail.com>
2024-01-15 10:49:52 -0300

    commit for debuging bank

--- of/STM.cu
+++ of/STM.cu
@@ -359,8 +359,8 @@ __device__  int* TX_Open_Write(STMData* stm_data, TX_Data* tx_data, uint object)
       Locator *new_locator = &stm_data -> locators[addr_new_locator];
       new_locator -> owner = tx_data->tr_id;
       new_locator -> object = object;
-      //printf("2queue[2969] = %d\n",tx_data -> locator_queue[2969]) ;
-      //printf("OW: Object %d, Transaction: %d Locator: %d (Owner %d), new Locator %d, next locator %d, queue %d\n",object,tx_data->tr_id, addr_locator,locator -> owner,addr_new_locator, next_locator,tx_data->next_locator);
+      printf("w2queue[2969] = %d\n",tx_data -> locator_queue[2969]) ;
+      printf("OW: Object %d, Transaction: %d Locator: %d (Owner %d), new Locator %d, next locator %d, queue %d\n",object,tx_data->tr_id, addr_locator,locator -> owner,addr_new_locator, next_locator,tx_data->next_locator);
       assert(locator -> owner != new_locator -> owner);
       
       switch (stm_data->tr_state[locator -> owner]) {
